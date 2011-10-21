cb1596f7e57fd0e2e2681b70000f0cdbfdbdabb4
Alasdair Gray <A.Gray@cs.man.ac.uk>
2011-10-21 17:01:21 +0100

    Initial implementation of IRS with exceptions thrown for each method

--- src/main/java/uk/ac/manchester/cs/IRSImpl.java
+++ src/main/java/uk/ac/manchester/cs/IRSImpl.java
@@ -1,11 +1,52 @@
 package uk.ac.manchester.cs;
 
+import java.net.URI;
+
 public class IRSImpl implements IRS {
-    
+
     @Override
-    public String hello(String name)
-    {
-        return "Hello " + name + "!";
+    public String getMappingsWithSubject(URI termURI) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
+    @Override
+    public String getMappingsWithSubject(URI termURI, URI profile) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getMappingsWithSubject(URI termURI, URI profile, int limit) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getMappingsWithSubject(URI termURI, int limit) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getMappingsWithTarget(URI termURI) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getMappingsWithTarget(URI termURI, URI profile) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getMappingsWithTarget(URI termURI, URI profile, int limit) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getMappingsWithTarget(URI termURI, int limit) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getMappingDetails(URI mappingURI) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
 }
