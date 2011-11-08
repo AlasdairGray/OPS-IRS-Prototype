62b6fa3191b3822cf1ffaea5b5cc74ed225f9ae1
Alasdair Gray <A.Gray@cs.man.ac.uk>
2011-11-08 11:49:10 +0000

    Insert RDF turtle linkset file into the mapping table. Tidied up error handling.

--- src/main/java/uk/ac/manchester/cs/irs/datastore/IRSDatabaseCreation.java
+++ src/main/java/uk/ac/manchester/cs/irs/datastore/IRSDatabaseCreation.java
@@ -5,10 +5,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.RDFParseException;
+import org.openrdf.rio.RDFParser;
+import org.openrdf.rio.helpers.StatementCollector;
+import org.openrdf.rio.turtle.TurtleParser;
 import uk.ac.manchester.cs.irs.IRSException;
 
 public class IRSDatabaseCreation {
@@ -26,25 +33,60 @@ public class IRSDatabaseCreation {
         try {
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
         } catch (SQLException ex) {
-            Logger.getLogger(MySQLAccess.class.getName()).log(Level.SEVERE, null, ex);
-            throw new IRSException("Problem connecting to database.", ex);
+            String msg = "Problem connecting to database";
+            Logger.getLogger(MySQLAccess.class.getName()).log(Level.SEVERE, msg, ex);
+            throw new IRSException(msg, ex);
         }
     }
     
-    private void createDatabase() {
+    private void createDatabase() throws IRSException {
         try {
             String sql = readFile("scripts/createMappingTable.sql");
-            Statement st = conn.createStatement();
+            java.sql.Statement st = conn.createStatement();
             st.execute("DROP TABLE mapping");
             st.execute(sql);
         } catch (SQLException ex) {
-            Logger.getLogger(MySQLAccess.class.getName()).log(Level.SEVERE, null, ex);
+            String msg = "Problem executing SQL statement to create database.";
+            Logger.getLogger(MySQLAccess.class.getName()).log(Level.SEVERE, msg, ex);
+            throw new IRSException(msg, ex);
         }
     }
+
+    private void insertRDFList(List<org.openrdf.model.Statement> rdfDataList) 
+            throws IRSException {
+        String insertStatement = "INSERT INTO IRS.mapping (source, predicate, target) "
+                + "VALUES(?, ?, ?)";
+        PreparedStatement insertMapping = null;
+        try {
+            insertMapping = conn.prepareStatement(insertStatement);
+            for (org.openrdf.model.Statement st : rdfDataList) {
+                insertMapping.setString(1, st.getSubject().stringValue());
+                insertMapping.setString(2, st.getPredicate().stringValue());
+                insertMapping.setString(3, st.getObject().stringValue());
+                insertMapping.executeUpdate();
+            }
+        }         
+        catch (SQLException ex) {
+            String msg = "Problem inserting values into database.";
+            Logger.getLogger(IRSDatabaseCreation.class.getName()).log(Level.SEVERE, msg, ex);
+            throw new IRSException(msg, ex);
+        } finally {
+            if (insertMapping != null) { 
+                try {
+                    insertMapping.close();
+                } catch (SQLException ex) {
+                    String msg = "Problem closing prepared insert statement.";
+                    Logger.getLogger(IRSDatabaseCreation.class.getName()).log(Level.SEVERE, msg, ex);
+                    throw new IRSException(msg, ex);
+                }
+            }
+        } 
+    }
     
-    private String readFile(String file) {
+    private String readFile(String fileName) 
+            throws IRSException {
         {
-            String fileLocation = this.getClass().getClassLoader().getResource(file).getPath();
+            String fileLocation = getFileLocation(fileName);
             System.out.println("File location:" + fileLocation);
             FileReader fileReader = null;
             StringBuilder sb = new StringBuilder();
@@ -57,21 +99,69 @@ public class IRSDatabaseCreation {
                 }
                 in.close();
             } catch (IOException ex) {
-                Logger.getLogger(MySQLAccess.class.getName()).log(Level.SEVERE, null, ex);
+                String msg = "Problem reading file " + fileName;
+                Logger.getLogger(MySQLAccess.class.getName()).log(Level.SEVERE, msg, ex);
+                throw new IRSException(msg, ex);
             } finally {
                 try {
                     fileReader.close();
                 } catch (IOException ex) {
-                    Logger.getLogger(MySQLAccess.class.getName()).log(Level.SEVERE, null, ex);
+                    String msg = "Problem closing file " + fileName;
+                    Logger.getLogger(MySQLAccess.class.getName()).log(Level.SEVERE, msg, ex);
+                    throw new IRSException(msg, ex);
                 }
             }
             return sb.toString();
+        }
+    }
+
+    private String getFileLocation(String fileName) {
+        String fileLocation = this.getClass().getClassLoader().getResource(fileName).getPath();
+        return fileLocation;
+    }
+
+    private void loadDataFromFile(String fileName, String baseURI) 
+            throws IRSException {
+        FileReader fileReader = null;
+        try {
+            String fileLocation = getFileLocation(fileName);
+            fileReader = new FileReader(fileLocation);
+            List<org.openrdf.model.Statement> rdfDataList = 
+                    new ArrayList<org.openrdf.model.Statement>();
+            RDFParser rdfParser = new TurtleParser();
+            StatementCollector collector = new StatementCollector(rdfDataList);
+            rdfParser.setRDFHandler(collector);
+            rdfParser.parse(fileReader, baseURI);
+            insertRDFList(rdfDataList);
+        } catch (IOException ex) {
+            String msg = "Problem reading file " + fileName;
+            Logger.getLogger(IRSDatabaseCreation.class.getName()).log(Level.SEVERE, msg, ex);
+            throw new IRSException(msg, ex);
+        } catch (RDFParseException ex) {
+            String msg = "Problem parsing file " + fileName;
+            Logger.getLogger(IRSDatabaseCreation.class.getName()).log(Level.SEVERE, msg, ex);
+            throw new IRSException(msg, ex);
+        } catch (RDFHandlerException ex) {
+            String msg = "Problem processing RDF in file " + fileName;
+            Logger.getLogger(IRSDatabaseCreation.class.getName()).log(Level.SEVERE, msg, ex);
+            throw new IRSException(msg, ex);
+        } finally {
+            try {
+                fileReader.close();
+            } catch (IOException ex) {
+                String msg = "Problem closing file " + fileName;
+                Logger.getLogger(IRSDatabaseCreation.class.getName()).log(Level.SEVERE, msg, ex);
+                throw new IRSException(msg, ex);
+            }
         }
     }
     
     public static void main(String[] args) throws IRSException {
         IRSDatabaseCreation dbCreator = new IRSDatabaseCreation();
         dbCreator.createDatabase();
+        dbCreator.loadDataFromFile(
+                "linksets/brenda_uniprot_has_ec_number_linkset.ttl",
+                "http://brenda-enzymes.info/");
     }
     
 }
