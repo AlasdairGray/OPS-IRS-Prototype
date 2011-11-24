ddf14b6ce2d84edce94154fd57a7b2e096c7215b
Alasdair Gray <A.Gray@cs.man.ac.uk>
2011-11-24 15:26:53 +0100

    Added use of one-jar maven plugin to generate a single executable jar that contains all of the dependencies

--- irs-db-admin/src/main/java/uk/ac/manchester/cs/irs/datastore/IRSDatabaseAdministration.java
+++ irs-db-admin/src/main/java/uk/ac/manchester/cs/irs/datastore/IRSDatabaseAdministration.java
@@ -1,9 +1,7 @@
 package uk.ac.manchester.cs.irs.datastore;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
