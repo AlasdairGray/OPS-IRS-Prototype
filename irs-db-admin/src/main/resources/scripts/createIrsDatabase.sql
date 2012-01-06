f5ec796e8358f0f8f089a58320c9c6c13b7e3ce4
Alasdair Gray <A.Gray@cs.man.ac.uk>
2012-01-06 14:15:15 +0000

    Added definition of linkset table
    Includes addition of indexes on source and target of mappings table as well as a foreign key reference to the linkset table.

--- irs-db-admin/src/main/resources/scripts/createIrsDatabase.sql
+++ irs-db-admin/src/main/resources/scripts/createIrsDatabase.sql
@@ -1,9 +1,25 @@
-DROP TABLE mapping;
+DROP TABLE IF EXISTS linkset;
+
+CREATE TABLE linkset (
+    id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    subjectsTarget VARCHAR(2083),
+    objectsTarget VARCHAR(2083),
+    linkPredicate VARCHAR(2083),
+    dateCreated DATE,
+    creator VARCHAR(2083),
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS mapping;
 
 CREATE TABLE mapping (
     id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    linkset_id MEDIUMINT NOT NULL,
     source VARCHAR(2083),
     predicate VARCHAR(2083),
     target VARCHAR(2083),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    CONSTRAINT fk_mapping_linkset FOREIGN KEY (linkset_id) REFERENCES linkset (id) ON DELETE CASCADE,
+    INDEX (source),
+    INDEX (target)
 );
